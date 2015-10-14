86b19ea2054a9207df15aa779909ac073d7d2598
fhanspach <f.hanspach@cuescience.de>
2015-10-14 13:19:09 +0200

    fixed uniqueness constraint error minor refactorings

--- reddit_api/imports/import_reddits.py
+++ reddit_api/imports/import_reddits.py
@@ -10,18 +10,20 @@ HEADER = {"User-Agent": settings.REDDIT_API_USER_AGENT}
 class RedditImporter(object):
     def __init__(self):
         self.last_request = datetime.datetime.now()
+        self.after = None
 
     def _save_reddit(self, reddit):
         new_reddit, _ = Reddit.objects.update_or_create(
-            title=reddit['title'],
-            display_name=reddit['display_name'],
-            description=reddit['description'],
-            over18=reddit['over18'],
-            public_description=reddit['public_description'],
-            date_created=datetime.datetime.fromtimestamp(reddit['created']),
             url=reddit['url'],
-            subscribers=reddit['subscribers'],
-            banner_image=reddit['banner_img'],
+            defaults={
+                "title": reddit['title'],
+                "display_name": reddit['display_name'],
+                "description": reddit['description'],
+                "over18": reddit['over18'],
+                "public_description": reddit['public_description'],
+                "date_created": datetime.datetime.fromtimestamp(reddit['created']),
+                "subscribers": reddit['subscribers'],
+                "banner_image": reddit['banner_img']}
         )
         new_reddit.save()
         print(new_reddit.title)
@@ -47,17 +49,18 @@ class RedditImporter(object):
         response_json = self._send_request(url)
         self._save_reddit(response_json['data'])
 
-    def import_all_reddits(self, after=None):
-        print("import started: {}".format(after))
+    def import_all_reddits(self):
+        print("import started: {}".format(self.after))
 
-        url = settings.ALL_REDDITS_URL.format(settings.IMPORT_BATCH_SIZE, "&after={}".format(after) if after else "")
+        url = settings.ALL_REDDITS_URL.format(settings.IMPORT_BATCH_SIZE,
+                                              "&after={}".format(self.after) if self.after else "")
         response_json = self._send_request(url)
 
         for reddit_data in response_json['data']['children']:
             reddit_data = reddit_data['data']
             self._save_reddit(reddit_data)
 
-        new_after = response_json['data']['after']
-        if new_after == "" or new_after is None:
+        self.after = response_json['data']['after']
+        if self.after == "" or self.after is None:
             return
-        self.import_all_reddits(after=new_after)
+        self.import_all_reddits()
