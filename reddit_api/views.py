27d2130c887d52fa11d44e557e6cec43d8dd8e07
fhanspach <f.hanspach@cuescience.de>
2015-10-16 23:08:05 +0200

    first version of design for text posts

--- reddit_api/views.py
+++ reddit_api/views.py
@@ -9,6 +9,34 @@ from requests import ConnectionError
 r = praw.Reddit(user_agent=settings.REDDIT_API_USER_AGENT)
 
 
+def _send_request(reddit_name, display='hot', period=None):
+    if display == 'new':
+        request_submissions = r.get_subreddit(reddit_name).get_new
+    elif display == 'hot' or display == "":
+        request_submissions = r.get_subreddit(reddit_name).get_hot
+    elif display == 'top':
+        if period == "week" or period is None:
+            request_submissions = r.get_subreddit(reddit_name).get_top
+        elif period == "month":
+            request_submissions = r.get_subreddit(reddit_name).get_top_from_month
+        elif period == "year":
+            request_submissions = r.get_subreddit(reddit_name).get_top_from_year
+        elif period == "all":
+            request_submissions = r.get_subreddit(reddit_name).get_top_from_all
+        elif period == "day":
+            request_submissions = r.get_subreddit(reddit_name).get_top_from_day
+        else:
+            raise ValueError("Period parameter unknown")
+    else:
+        raise ValueError("Display Parameter must be one of 'hot', 'top' or 'new'")
+
+    try:
+        api_submissions = request_submissions(limit=100, params={})
+    except ConnectionError:
+        api_submissions = []
+    return api_submissions
+
+
 def get_submissions(request, reddit_name):
     reddit = Reddit.objects.get(url="/r/{}/".format(reddit_name))
     reddit_image = reddit.image
@@ -18,12 +46,8 @@ def get_submissions(request, reddit_name):
         reddit.image = reddit_image
         reddit.save()
 
-    try:
-        api_submissions = r.get_subreddit(reddit_name).get_new(
-            limit=100, params={}
-        )
-    except ConnectionError:
-        api_submissions = []
+    api_submissions = _send_request(reddit_name, display=request.GET.get('display', 'hot'),
+                                    period=request.GET.get('period', None))
 
     entry_list = []
     for submission in api_submissions:
@@ -35,7 +59,8 @@ def get_submissions(request, reddit_name):
             reddit_image.reputation = submission.ups
 
     context = {
-        'submissions': entry_list
+        'submissions': entry_list,
+        'reddit': reddit
     }
     html = render_to_string('reddit_api/submissions.html', RequestContext(request, context))
     reddit_image.save()
