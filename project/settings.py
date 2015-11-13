afa84168b746a5b921fb2384a82a1dbbccbad784
fhanspach <f.hanspach@cuescience.de>
2015-11-13 21:51:26 +0100

    added dj_database_url to database config added django flat theme to requirements

--- project/settings.py
+++ project/settings.py
@@ -82,13 +82,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
